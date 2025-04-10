3cda2f9e0e74989caa79b1a5d7444a1595491ed3
Aarya Patel <163074108+Aarya-Patel7@users.noreply.github.com>
2025-04-10 13:25:41 -0500

    Update Price.java

--- Price.java
+++ Price.java
@@ -1,4 +1,4 @@
-/** Super class of the different type of movies' prices.
+/*  Super class of the different type of movies' prices.
     Provides method structure for calculating and returning the rental fee per movie
     and the amount of renter points given per movie.
 */
