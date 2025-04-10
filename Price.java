d063c9cc7b3612102ad19b17ad6af2867f03eca7
Aarya Patel <163074108+Aarya-Patel7@users.noreply.github.com>
2025-04-10 13:23:31 -0500

    added comment(s)

--- Price.java
+++ Price.java
@@ -1,3 +1,7 @@
+/** Super class of the different type of movies' prices.
+    Provides method structure for calculating and returning the rental fee per movie
+    and the amount of renter points given per movie.
+*/
 public abstract class Price {
     public abstract double getCharge(int daysRented);
     public int getFrequentRenterPoints(int daysRented) {
