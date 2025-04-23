165acc67ae6c856c82972cc3ad9dc246c432ab72
Aarya Patel <163074108+Aarya-Patel7@users.noreply.github.com>
2025-04-23 12:24:31 -0500

    Delete StandardPointsStrategy.java

--- StandardPointsStrategy.java
+++
@@ -1,9 +0,0 @@
-/**
- * Standard strategy for calculating frequent renter points (1 point per rental)
- */
-public class StandardPointsStrategy implements FrequentRenterPointsStrategy {
-    @Override
-    public int getFrequentRenterPoints(int daysRented) {
-        return 1;
-    }
-}
