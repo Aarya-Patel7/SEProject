e1e54e67e38d965b922d91d95ef17a0f0170f114
Aarya Patel <163074108+Aarya-Patel7@users.noreply.github.com>
2025-04-24 16:43:00 -0500

    Update ChildrenFrequentRentalPoints.java

--- ChildrenFrequentRentalPoints.java
+++ ChildrenFrequentRentalPoints.java
@@ -1,4 +1,4 @@
-// Frequent rentla points stratergy for children movies
+// Frequent rental points strategy for children movies
 public class ChildrenFrequentRentalPoints implements FrequentRentalStratergy{
     public int getFrequentRenterPoints(Rental r) {
         return 1;
