6f517da609786d52a6f349f7c17bd844712140d6
Aarya Patel <163074108+Aarya-Patel7@users.noreply.github.com>
2025-04-24 16:44:05 -0500

    Update FrequentRentalStratergy.java

--- FrequentRentalStratergy.java
+++ FrequentRentalStratergy.java
@@ -1,4 +1,4 @@
-// Stratergy to extend on to calulate frequenct rental points for different types of movies
+// Strategy to extend on to calulate frequent rental points for different types of movies
 public interface FrequentRentalStratergy {
     public int getFrequentRenterPoints(Rental r);
 }
